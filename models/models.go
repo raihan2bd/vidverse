a8022e5d19614107d4ceaadead6e1278e99d69f9
raihan2bd <raihan2bd.official@gmail.com>
2023-09-22 20:16:21 +0600

    Add user and video model

---
+++ models/models.go
@@ -0,0 +1,17 @@
+package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Name     string
+	Email    string
+	Password string `json:"-"`
+}
+
+type Video struct {
+	gorm.Model
+	Title       string
+	Description string
+	PublicID    string
+}
