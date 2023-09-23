1b7586671d40a1477e63eb16e47b97d1932084fd
raihan2bd <raihan2bd.official@gmail.com>
2023-09-23 14:06:59 +0600

    Add necessary routes for video

--- cmd/api/route.go
+++ cmd/api/route.go
@@ -6,9 +6,14 @@ import (
 
 func NewRouter() *gin.Engine {
 	r := gin.New()
+	r.Use(gin.Logger())
 
 	v1 := r.Group("/api/v1")
 	v1.GET("/", GetStatus)
+	v1.POST("/uploads/video", UploadVideo)
+	v1.DELETE("/videos/:videoID", DeleteVidoe)
+	v1.GET("/videos/:videoID", GetSingleVideo)
+	v1.GET("/file/video/:videoID", StreamVideo)
 
 	return r
 }
