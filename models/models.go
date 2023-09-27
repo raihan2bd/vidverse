d8a4f62598706b1e0bf62bd131dc003b1e11e706
raihan2bd <raihan2bd.official@gmail.com>
2023-09-27 19:50:11 +0600

    Refactor Models and also add necessary associations

--- models/models.go
+++ models/models.go
@@ -14,16 +14,48 @@ type CustomModel struct {
 }
 
 type User struct {
-	CustomModel
-	Name     string
-	Email    string
-	Password string `json:"-"`
+	gorm.Model
+	Name      string  `gorm:"type:varchar(100);not null" json:"name" binding:"required,min=3,max=100"`
+	Email     string  `gorm:"type:varchar(255);unique;not null" json:"email" binding:"required,email"`
+	Password  string  `gorm:"type:varchar(255);not null" json:"-"`
+	ChannelID uint    `json:"channel_id"`
+	Channel   Channel `gorm:"foreignKey:ChannelID" json:"channel"`
+}
+
+type Channel struct {
+	gorm.Model
+	Title       string  `gorm:"type:varchar(100)" json:"title" binding:"required,min=5,max=100"`
+	Description string  `gorm:"type:text;size:500" json:"description" binding:"required,min=25,max=100"`
+	UserID      uint    `json:"user_id"`
+	Videos      []Video `gorm:"foreignKey:ChannelID" json:"videos"`
 }
 
 type Video struct {
-	CustomModel
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	PublicID    string `json:"-"`
-	SecureURL   string `json:"secure_url"`
+	gorm.Model
+	Title       string    `gorm:"type:varchar(255);not null" json:"title" binding:"required,min=2,max=255"`
+	Description string    `gorm:"type:text;size:500;not null" json:"description" binding:"required,min=2,max=500"`
+	PublicID    string    `gorm:"type:varchar(255);not null" json:"-"`
+	SecureURL   string    `gorm:"type:varchar(255);not null" json:"secure_url"`
+	ChannelID   uint      `json:"channel_id"`
+	Channel     Channel   `gorm:"foreignKey:ChannelID" json:"channel"`
+	Thumb       string    `gorm:"type:varchar(255)" json:"tumb"`
+	Likes       []Like    `json:"likes"`
+	Comments    []Comment `json:"comments"`
+}
+
+type Like struct {
+	gorm.Model
+	UserID  uint  `json:"user_id"`
+	VideoID uint  `json:"video_id"`
+	Video   Video `gorm:"foreignKey:VideoID"`
+	User    User  `gorm:"foreignKey:UserID"`
+}
+
+type Comment struct {
+	gorm.Model
+	Text    string `gorm:"type:text;size:500" json:"text"`
+	UserID  uint   `json:"user_id"`
+	VideoID uint   `json:"video_id"`
+	Video   Video  `gorm:"foreignKey:VideoID"`
+	User    User   `gorm:"foreignKey:UserID"`
 }
