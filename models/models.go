834e50a460e117d558810460bfd625f67a8d23f3
raihan2bd <raihan2bd.official@gmail.com>
2023-10-29 11:55:14 +0600

    Modify data models

--- models/models.go
+++ models/models.go
@@ -18,14 +18,28 @@ type User struct {
 	Name     string `gorm:"type:varchar(100);not null" json:"name"`
 	UserName string `gorm:"type:varchar(100);unique;not null" json:"username"`
 	Email    string `gorm:"type:varchar(255);unique;not null" json:"email"`
-	Password string `gorm:"type:varchar(255);not null" json:"password,omitempty"`
+	Password string `gorm:"type:varchar(255);not null" json:"-"`
 	Avatar   string `gorm:"type:varchar(255);not null;default:'https://upload.wikimedia.org/wikipedia/commons/5/59/User-avatar.svg'" json:"avatar"`
+	IsActive bool   `gorm:"type:boolean;not null;default:false" json:"is_active"`
+	UserRole string `gorm:"type:varchar(150);not null;default:'user'" json:"user_role"`
+}
+
+type UserPayload struct {
+	CustomModel
+	Name     string `gorm:"type:varchar(100);not null" json:"name"`
+	UserName string `gorm:"type:varchar(100);unique;not null" json:"username"`
+	Email    string `gorm:"type:varchar(255);unique;not null" json:"email"`
+	Password string `gorm:"type:varchar(255);not null" json:"password"`
+	Avatar   string `gorm:"type:varchar(255);not null;default:'https://upload.wikimedia.org/wikipedia/commons/5/59/User-avatar.svg'" json:"avatar"`
+	IsActive bool   `gorm:"type:boolean;not null;default:false" json:"is_active"`
+	UserRole string `gorm:"type:varchar(150);not null;default:'user'" json:"user_role"`
 }
 
 type Channel struct {
 	CustomModel
-	Title       string  `gorm:"type:varchar(100)" json:"title" binding:"required,min=5,max=100"`
-	Description string  `gorm:"type:text;size:500" json:"description" binding:"required,min=25,max=100"`
+	Title       string  `gorm:"type:varchar(100)" json:"title"`
+	Description string  `gorm:"type:text;size:500" json:"description"`
+	Logo        string  `gorm:"type:varchar(255);" json:"logo"`
 	UserID      uint    `json:"user_id"`
 	User        User    `gorm:"foreignKey:UserID" json:"user"`
 	Videos      []Video `gorm:"foreignKey:ChannelID" json:"videos"`
