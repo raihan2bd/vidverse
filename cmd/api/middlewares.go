da0b61ce5d7accd9b6aaadbc7b93cc3f2716c0f8
raihan2bd <raihan2bd.official@gmail.com>
2023-11-23 20:20:09 +0600

    Add middlewares into the project to authenticate and authorize the user

--- cmd/api/middlewares.go
+++ cmd/api/middlewares.go
@@ -1,18 +1,69 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
+	"github.com/raihan2bd/vidverse/helpers"
 )
 
-func (app *application) IsLoggedIn(c *gin.Context) {
-	tokenString, err := c.Cookie("token")
-	if err != nil {
-		fmt.Println(err)
+// pass the user_id if logged in
+func (app *application) HasToken(c *gin.Context) {
+	token := c.Request.Header.Get("Authorization")
+	if token == "" {
+		c.Next()
 		return
 	}
 
-	fmt.Println("Logged-In user", tokenString)
+	claims, err := helpers.DecodeToken(token)
+
+	if err != nil {
+		c.Next()
+		return
+	}
+
+	if !helpers.ValidateToken(claims) {
+		c.Next()
+		return
+	}
+
+	c.Set("user_id", claims["sub"])
+	c.Next()
+}
+
+func (app *application) IsLoggedIn(c *gin.Context) {
+	claims, err := helpers.DecodeToken(c.Request.Header.Get("Authorization"))
+
+	if err != nil {
+		c.AbortWithStatus(401)
+		return
+	}
+
+	if !helpers.ValidateToken(claims) {
+		c.AbortWithStatus(401)
+		return
+	}
+
+	c.Set("user_id", claims["sub"])
+	c.Next()
+}
+
+func (app *application) IsAdmin(c *gin.Context) {
+	claims, err := helpers.DecodeToken(c.Request.Header.Get("Authorization"))
+
+	if err != nil {
+		c.AbortWithStatus(401)
+		return
+	}
+
+	if !helpers.ValidateToken(claims) {
+		c.AbortWithStatus(401)
+		return
+	}
+
+	if claims["user_role"] != "admin" {
+		c.AbortWithStatus(403)
+		return
+	}
+
+	c.Set("user_id", claims["sub"])
 	c.Next()
 }
