f79e11bbc6982b346d5535f3d2d0bc848095d7c8
raihan2bd <raihan2bd.official@gmail.com>
2023-12-01 12:58:14 +0600

    Add a route to get all videos by channel id

--- cmd/api/route.go
+++ cmd/api/route.go
@@ -22,6 +22,7 @@ func (app *application) NewRouter() *gin.Engine {
 	v1.POST("/auth/signup", app.SignupHandler)
 	v1.GET("/videos", app.HandleGetAllVideos)
 	v1.POST("/videos", app.IsAdmin, UploadVideo)
+	v1.GET("/get_videos/:channelID", app.HandleGetVideosByChannelID)
 	v1.GET("/videos/:videoID", app.HandleGetSingleVideo)
 	v1.DELETE("/videos/:videoID", app.HandleDeleteVidoe)
 	v1.GET("/related_videos/:channelID", app.HandleGetRelatedVideos)
