57c1912ba799f94e7f9c64cd836cfff948894a4a
raihan2bd <raihan2bd.official@gmail.com>
2024-02-09 16:58:56 +0600

    Add mailer service to the application config

--- config/config.go
+++ config/config.go
@@ -1,9 +1,13 @@
 package config
 
 import (
+	"os"
+	"strconv"
+
 	"github.com/cloudinary/cloudinary-go/v2"
 	"github.com/gorilla/websocket"
 	"github.com/raihan2bd/vidverse/initializers"
+	"github.com/raihan2bd/vidverse/internal/mail"
 	"github.com/raihan2bd/vidverse/repository"
 	dbrepo "github.com/raihan2bd/vidverse/repository/dbRepo"
 	"gorm.io/gorm"
@@ -14,6 +18,7 @@ type Application struct {
 	CLD              *cloudinary.Cloudinary
 	DBMethods        repository.DatabaseRepo
 	NotificationChan chan *NotificationEvent
+	Mailer           mail.Mail
 }
 
 type NotificationEvent struct {
@@ -45,10 +50,23 @@ func LoadConfig() (*Application, error) {
 		return nil, err
 	}
 
+	mailPort, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	m := mail.Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        mailPort,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRIPTION"),
+		FromName:    os.Getenv("MAIL_FROM_NAME"),
+		FromAddress: os.Getenv("MAIL_FROM_ADDRESS"),
+	}
+
 	return &Application{
 		DB:               db,
 		DBMethods:        dbrepo.NewPostgresRepo(initializers.DB, initializers.CLD),
 		CLD:              cld,
 		NotificationChan: make(chan *NotificationEvent),
+		Mailer:           m,
 	}, nil
 }
